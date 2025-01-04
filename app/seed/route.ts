114934dcbaed7b6d3df3044881b799eb2afa917b
JohnnyButloe <jbutl003@gmail.com>
2025-01-04 12:47:50 -0500

    DB seed(2)

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,11 +101,6 @@ async function seedRevenue() {
   return insertedRevenue;
 }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
