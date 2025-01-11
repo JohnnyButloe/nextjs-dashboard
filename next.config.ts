37714ca1dc93f38acb209b87f88a4b5b40c814f4
JohnnyButloe <jbutl003@gmail.com>
2025-01-11 17:22:47 -0500

    rmPPR

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-// import type { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
- 
-const nextConfig = {
-  experimental: {
-    ppr: 'incremental',
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
- 
+
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+ 
+// const nextConfig = {
+//   experimental: {
+//     ppr: 'incremental',
+//   },
+// };
+ 
+// export default nextConfig;
